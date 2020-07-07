c009338c063fdae1d1d47b8c4d01fe5031a6feec
David <davidhowell.tx@gmail.com>
2020-07-07 12:31:51 -0500

    Update Get-S1Exclusion

--- PS-SentinelOne/Exclusions/Get-S1Exclusion.ps1
+++ PS-SentinelOne/Exclusions/Get-S1Exclusion.ps1
@@ -1,20 +1,63 @@
 function Get-S1Exclusion {
     <#
     .SYNOPSIS
-        Retrieves information related to SentinelOne exclusions 
-    
-
-
-    .NOTES Options Implemented
-        
-    .NOTES Options not yet implemented 
-         
+        Retrieves information related to SentinelOne exclusions
     #>
     [CmdletBinding(DefaultParameterSetName="All")]
     Param(
         [Parameter(Mandatory=$False)]
-        [Switch]
+        [ValidateSet(
+            "path",
+            "certificate",
+            "browser",
+            "file_type",
+            "white_hash"
+        )]
+        [String]
+        $Type,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateSet("true","false")]
+        [String]
         $IncludeInherited,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateSet(
+            "windows",
+            "windows_legacy",
+            "macos",
+            "linux")]
+        [String[]]
+        $OSType,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateSet(
+            "suppress",
+            "suppress_dynamic_only",
+            "suppress_dfi_only",
+            "disable_in_process_monitor",
+            "disable_in_process_monitor_deep",
+            "disable_all_monitors",
+            "disable_all_monitors_deep"
+        )]
+        [String[]]
+        $Mode,
+
+        [Parameter(Mandatory=$False)]
+        [String]
+        $Value,
+
+        [Parameter(Mandatory=$False)]
+        [String]
+        $Search,
+
+        [Parameter(Mandatory=$False)]
+        [String[]]
+        $ExclusionID,
+
+        [Parameter(Mandatory=$False)]
+        [String[]]
+        $UserID,
 
         [Parameter(Mandatory=$False)]
         [String[]]
@@ -26,21 +69,7 @@ function Get-S1Exclusion {
 
         [Parameter(Mandatory=$False)]
         [String[]]
-        $AccountID,
-
-        [Parameter(Mandatory=$False)]
-        [String[]]
-        $UserID,
-
-        [Parameter(Mandatory=$False)]
-        [ValidateSet("windows", "windows_legacy", "macos", "linux")]
-        [String[]]
-        $OSType,
-
-        [Parameter(Mandatory=$False)]
-        [ValidateSet("suppress", "suppress_dynamic_only", "suppress_dfi_only", "disable_in_process_monitor", "disable_in_process_monitor_deep", "disable_all_monitors", "disable_all_monitors_deep")]
-        [String[]]
-        $Mode
+        $AccountID
     )
     Process {
         # Log the function and parameters being executed
@@ -48,20 +77,20 @@ function Get-S1Exclusion {
         $MyInvocation.BoundParameters.GetEnumerator() | ForEach-Object { $InitializationLog = $InitializationLog + " -$($_.Key) $($_.Value)"}
         Write-Log -Message $InitializationLog -Level Informational
         
-        $URI = "/web/api/v2.0/restrictions"
+        $URI = "/web/api/v2.1/restrictions"
         $Parameters = @{}
-        #$Parameters.Add("limit", 50)
-        $Parameters.Add("type", "black_hash" )
-
-
+        
+        if ($Type) { $Parameters.Add("type", $Type) }
+        if ($IncludeInherited) { $Parameters.Add("unified", $IncludeInherited) }
+        if ($OSType) { $Parameters.Add("osTypes", ($OSType -join ",") ) }
+        if ($Mode) { $Parameters.Add("modes", ($Mode -join ",") ) }
+        if ($Value) { $Parameters.Add("value", $Value) }
+        if ($Search) { $Parameters.Add("query", $Search) }
+        if ($ExclusionID) { $Parameters.Add("ids", ($ExclusionID -join ",") ) }
+        if ($UserID) { $Parameters.Add("userIds", ($UserID -join ",") ) }
         if ($GroupID) { $Parameters.Add("groupIds", ($GroupID -join ",") ) }
         if ($SiteID) { $Parameters.Add("siteIds", ($SiteID -join ",") ) }
         if ($AccountID) { $Parameters.Add("accountIds", ($AccountID -join ",") ) }
-        if ($UserID) { $Parameters.Add("userIds", ($UserID -join ",") ) }
-        if ($OSType) { $Parameters.Add("osTypes", ($OSType -join ",") ) }
-        if ($Mode) { $Parameters.Add("modes", ($Mode -join ",") ) }
-        if ($IncludeInherited) { $Parameters.Add("unified", $IncludeInherited) }
-
         
         $Response = Invoke-S1Query -URI $URI -Method GET -Parameters $Parameters -Recurse
         Write-Output $Response.data
