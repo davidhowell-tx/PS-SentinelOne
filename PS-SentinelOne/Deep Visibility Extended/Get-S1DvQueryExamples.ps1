9f9de439b4256d6bf5c03d69ef300d0914bd823a
Howell <David.Howell@us.hctx.net>
2020-11-10 09:39:54 -0600

    Initializing commandlet to return DV Query Examples. Needs work

---
+++ PS-SentinelOne/Deep Visibility Extended/Get-S1DvQueryExamples.ps1
@@ -0,0 +1,4 @@
+function Get-S1DvQueryExamples {
+    "DNSRequest In Contains Anycase (`"" + ($SearchValue -join "`",`"") + "`")"
+    "DNSResponse In Contains Anycase (`"" + ($SearchValue -join "`",`"") + "`")"
+}
