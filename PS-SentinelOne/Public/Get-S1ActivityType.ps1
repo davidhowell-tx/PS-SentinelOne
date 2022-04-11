80bdf71c55d893947cc763f6ae6e0b791c2912b6
David <davidhowell.tx@gmail.com>
2022-04-11 12:04:32 -0500

    change init log level

--- PS-SentinelOne/Public/Get-S1ActivityType.ps1
+++ PS-SentinelOne/Public/Get-S1ActivityType.ps1
@@ -9,7 +9,7 @@ function Get-S1ActivityType {
         # Log the function and parameters being executed
         $InitializationLog = $MyInvocation.MyCommand.Name
         $MyInvocation.BoundParameters.GetEnumerator() | ForEach-Object { $InitializationLog = $InitializationLog + " -$($_.Key) $($_.Value)"}
-        Write-Log -Message $InitializationLog -Level Informational
+        Write-Log -Message $InitializationLog -Level Verbose
         
         $URI = "/web/api/v2.1/activities/types"
         $Response = Invoke-S1Query -URI $URI -Method GET -Recurse
