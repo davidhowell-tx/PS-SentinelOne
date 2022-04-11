5ac6ffbf52a71c5f23b65a3730d821f5c3de2040
David <davidhowell.tx@gmail.com>
2022-04-11 11:33:35 -0500

    Relocated parameter documentation

--- PS-SentinelOne/Private/Format-Date.ps1
+++ PS-SentinelOne/Private/Format-Date.ps1
@@ -6,9 +6,6 @@ function Format-Date {
     .DESCRIPTION
         Used to format a DateTime object into the various formats used by the SentinelOne API
         Default format is yyyy-MM-ddTHH:mm:ss.ffffffZ, i.e. 2020-01-31T06:00:00.000000Z
-    
-    .PARAMETER UnixMS
-        Used to change output to Unix Epoch time in Milliseconds
     #>
     [CmdletBinding()]
     Param(
@@ -16,15 +13,11 @@ function Format-Date {
         [DateTime]
         $InputObject,
 
+        #Used to change output to Unix Epoch time in Milliseconds
+        [Parameter(Mandatory=$False)]
         [Switch]
         $UnixMS
     )
-
-    # Log the function and parameters being executed
-    $InitializationLog = $MyInvocation.MyCommand.Name
-    $MyInvocation.BoundParameters.GetEnumerator() | ForEach-Object { $InitializationLog = $InitializationLog + " -$($_.Key) $($_.Value)"}
-    Write-Log -Message $InitializationLog -Level Verbose
-
     $InputObjectUTC = $InputObject.ToUniversalTime()
     if ($UnixMS) {
         $UnixSource = (Get-Date "1/1/1970").ToUniversalTime()
